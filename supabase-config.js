a194878300a1521fc73fedc802a241492292242f
tvlmedia <info@tvlmedia.nl>
2026-05-27 18:34:48 +0200

    Fix LensWiki Supabase project ref

--- supabase-config.js
+++ supabase-config.js
@@ -1,2 +1,2 @@
-window.LENSWIKI_SUPABASE_URL = "https://vfvxvmfkzbythgkupbks.supabase.co";
+window.LENSWIKI_SUPABASE_URL = "https://fvfxvmfkzbythgkupbks.supabase.co";
 window.LENSWIKI_SUPABASE_ANON_KEY = "sb_publishable_70E5vFoBfT1q6npGcWA5Xg_JYp0SZPF";
